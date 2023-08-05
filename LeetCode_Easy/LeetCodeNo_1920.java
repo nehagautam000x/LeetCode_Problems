1a8b985a7fbb1e5ed20f26a798673cb008f8c222
neha-gthm <write2nehagautam@gmail.com>
2023-08-05 18:22:40 +0530

    added and renamed files

---
+++ LeetCode_Easy/LeetCodeNo_1920.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/build-array-from-permutation/description/
+
+// 1920. Build Array from Permutation
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
---
+++ LeetCode_Easy/LeetCodeNo_1929.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/concatenation-of-array/description/
+
+// 1929. Concatenation of Array
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length*2];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+        }
+        return ans;
+    }
+}
